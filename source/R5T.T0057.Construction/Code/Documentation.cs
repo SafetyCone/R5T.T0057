1d640e33fca1319986511741d8e49bca0cf03251
davidcoats <david@getsafetycone.com>
2021-09-30 11:53:09 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0057.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0057.Construction
+{
+    /// <summary>
+    /// Construction project for DuplicateValuesOperator, IgnoredValuesOperator, and other value-related operator extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0057/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0057
+{
+    /// <summary>
+    /// DuplicateValuesOperator, IgnoredValuesOperator, and other value-related operator extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
